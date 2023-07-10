490a115c4dffb736db2a8d9c15d4ad9a6988f054
sb-bilal-dev <saidbilol18@gmail.com>
2023-07-10 22:18:58 +0500

    format

--- auth.js
+++ auth.js
@@ -1,38 +1,38 @@
 
 const users = [
-    { id: 1, username: 'admin', password: 'password' },
-    // Add more users here as needed
-] 
+  { id: 1, username: 'admin', password: 'password' },
+  // Add more users here as needed
+]
 
 function getUserToken(req, res) {
-    const matchedUser = users.find(u => {
-        req.headers.authorization
+  const matchedUser = users.find(u => {
+    req.headers.authorization
 
-        if (req.body)
-        if (u.username === req.body.username
-            && u.password === req.body.password) {
-            return true;
-        } else {
-            return false;
-        }
-    })
-    if (!matchedUser) {
-        return res.sendStatus(401)
-    } else {
-        req.headers.authorization = "4321431513431"
-        res.status(200).send().end()
-    }
+    if (req.body)
+      if (u.username === req.body.username
+        && u.password === req.body.password) {
+        return true;
+      } else {
+        return false;
+      }
+  })
+  if (!matchedUser) {
+    return res.sendStatus(401)
+  } else {
+    req.headers.authorization = "4321431513431"
+    res.status(200).send().end()
+  }
 }
 
 function checkUser(req, res, next) {
-    if (req.headers.authorization === "4321431513431") {
-        next()
-    } else {
-        return res.sendStatus(401)
-    }
+  if (req.headers.authorization === "4321431513431") {
+    next()
+  } else {
+    return res.sendStatus(401)
+  }
 }
 
 module.exports = {
-    checkUser,
-    getUserToken,
+  checkUser,
+  getUserToken,
 }
--- encryptTime.js
+++ encryptTime.js
@@ -1,40 +1,40 @@
 module.exports = function encryptTime() {
-    const now = new Date();
-    const hour = now.getHours();
-    const minute = now.getMinutes();
-    const roundedMinute = Math.floor(minute / 15) * 15;
-  
-    const encryptionNumbers = new Array(5);
-    let firstDigit = Math.floor(hour / 10) ^ Math.floor(roundedMinute / 10);
-    let secondDigit = (hour % 4) ^ (roundedMinute % 4);
-    let thirdDigit = Math.floor(hour / 10) ^ (roundedMinute % 10);
-    let fourthDigit = (hour % 10) ^ Math.floor(roundedMinute / 10);
-    let fifthDigit = (hour % 5) ^ Math.floor(roundedMinute / 5);
-  
-    firstDigit = Math.floor(hour / 10) ^ Math.floor(firstDigit / 10);
-    secondDigit = (hour % 4) ^ (secondDigit % 4);
-    thirdDigit = Math.floor(hour / 10) ^ (thirdDigit % 10);
-    fourthDigit = (hour % 10) ^ Math.floor(fourthDigit / 10);
-  
-    if (roundedMinute % 2 === 0) {
-      if (firstDigit < 5) firstDigit += 3;
-      if (secondDigit < 4) secondDigit += 5;
-      if (thirdDigit < 4) thirdDigit += 7;
-      if (fourthDigit < 5) fourthDigit += 9;
-      if (fifthDigit < 5) fifthDigit += 1;
-    } else {
-      if (firstDigit < 4) firstDigit += 5;
-      if (secondDigit < 5) secondDigit += 3;
-      if (thirdDigit < 5) thirdDigit += 8;
-      if (fourthDigit < 5) fourthDigit += 1;
-      if (fifthDigit < 4) fifthDigit += 9;
-    }
-  
-    encryptionNumbers[0] = firstDigit;
-    encryptionNumbers[1] = secondDigit;
-    encryptionNumbers[2] = thirdDigit;
-    encryptionNumbers[3] = fourthDigit;
-    encryptionNumbers[4] = fifthDigit;
-  
-    return encryptionNumbers;
+  const now = new Date();
+  const hour = now.getHours();
+  const minute = now.getMinutes();
+  const roundedMinute = Math.floor(minute / 15) * 15;
+
+  const encryptionNumbers = new Array(5);
+  let firstDigit = Math.floor(hour / 10) ^ Math.floor(roundedMinute / 10);
+  let secondDigit = (hour % 4) ^ (roundedMinute % 4);
+  let thirdDigit = Math.floor(hour / 10) ^ (roundedMinute % 10);
+  let fourthDigit = (hour % 10) ^ Math.floor(roundedMinute / 10);
+  let fifthDigit = (hour % 5) ^ Math.floor(roundedMinute / 5);
+
+  firstDigit = Math.floor(hour / 10) ^ Math.floor(firstDigit / 10);
+  secondDigit = (hour % 4) ^ (secondDigit % 4);
+  thirdDigit = Math.floor(hour / 10) ^ (thirdDigit % 10);
+  fourthDigit = (hour % 10) ^ Math.floor(fourthDigit / 10);
+
+  if (roundedMinute % 2 === 0) {
+    if (firstDigit < 5) firstDigit += 3;
+    if (secondDigit < 4) secondDigit += 5;
+    if (thirdDigit < 4) thirdDigit += 7;
+    if (fourthDigit < 5) fourthDigit += 9;
+    if (fifthDigit < 5) fifthDigit += 1;
+  } else {
+    if (firstDigit < 4) firstDigit += 5;
+    if (secondDigit < 5) secondDigit += 3;
+    if (thirdDigit < 5) thirdDigit += 8;
+    if (fourthDigit < 5) fourthDigit += 1;
+    if (fifthDigit < 4) fifthDigit += 9;
+  }
+
+  encryptionNumbers[0] = firstDigit;
+  encryptionNumbers[1] = secondDigit;
+  encryptionNumbers[2] = thirdDigit;
+  encryptionNumbers[3] = fourthDigit;
+  encryptionNumbers[4] = fifthDigit;
+
+  return encryptionNumbers;
 }
