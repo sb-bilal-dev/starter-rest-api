59806edac24f488e5e06e383dcac6c398af2d44f
sb-bilal-dev <saidbilol18@gmail.com>
2023-07-10 23:10:31 +0500

    cors lib`

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 const express = require('express')
 const app = express()
 const db = require('@cyclic.sh/dynamodb')
+const cors = require('cors')
 const encryptTime = require('./encryptTime')
 const { getUserToken, checkUser } = require('./auth')
 
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', "*");
-  next();
-});
 
 // #############################################################################
 // This configures static hosting for files in /public that have the extensions
