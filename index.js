3b61d32225ef386dd7c2d89473911adbe1c34f27
sb-bilal-dev <saidbilol18@gmail.com>
2023-07-10 22:39:37 +0500

    CORS all

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://7hy2q8.csb.app');
+  res.setHeader('Access-Control-Allow-Origin', "*");
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Credentials', true);
