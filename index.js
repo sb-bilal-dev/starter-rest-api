0ad55487b9c02ce4c0774d688a9720e040f0ad07
sb-bilal-dev <saidbilol18@gmail.com>
2023-07-19 20:20:30 +0500

    prefer statusMessage

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ app.post('/auth', getUserToken)
 app.get('/code', checkUser, (req, res) => {
   // Code for handling the protected route
   var unblockerCode = encryptTime(req.query.salt);
-  res.sendStatus(200).send(unblockerCode).end()
+  res.statusCode(200).statusMessage(unblockerCode).end()
 })
 
 // Delete an item
